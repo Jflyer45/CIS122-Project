b346c8219ea0186d8d48b8869a29b530322878be
Jeremy Fischer <jeremyfischer52@gmail.com>
2021-11-04 10:16:39 -0500

    Add Movement Right & Left

--- PlatformerGameCIS122/Assets/Scripts/PlayerController.cs
+++ PlatformerGameCIS122/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,33 @@
 // Created by: Jeremy Fischer 11/3/2021
-// Edited by (Add name if you do anything to this script): Jeremy Fischer,  
+// Edited by (Add name if you do anything to this script): Jeremy Fischer 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private Variables
+    private Rigidbody2D rb;
+    private Collider2D coll;
+
+    // Serialized and Public Variables
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
     }
 }
