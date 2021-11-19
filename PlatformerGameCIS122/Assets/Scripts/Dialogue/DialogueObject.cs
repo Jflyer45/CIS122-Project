75c21fd583e3c8c90b28041a3641fadcb21c4992
Jeremy Fischer <jeremyfischer52@gmail.com>
2021-11-19 11:41:06 -0600

    Work on responses

--- PlatformerGameCIS122/Assets/Scripts/Dialogue/DialogueObject.cs
+++ PlatformerGameCIS122/Assets/Scripts/Dialogue/DialogueObject.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class DialogueObject : ScriptableObject
 {
     [SerializeField] [TextArea] private string[] dialogue;
+    [SerializeField] private Response[] responses;
 
     // Get Sets
     // Read only
     public string[] Dialogue => dialogue;
+    public Response[] Responses => responses;
 }
