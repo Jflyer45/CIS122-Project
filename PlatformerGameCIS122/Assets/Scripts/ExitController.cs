90d3f28566c4c9f671bf308bbd4193707e0997bb
Jeremy Fischer <jeremyfischer52@gmail.com>
2021-12-07 10:55:18 -0600

    Create exit script and add functionality to level 1

---
+++ PlatformerGameCIS122/Assets/Scripts/ExitController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitController : MonoBehaviour
+{
+    [SerializeField] private string scenceDestination;
+
+    // Trigger Detection.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player touches the trophy
+        if(collision.tag == "Player")
+        {
+            SceneManager.LoadScene(scenceDestination);
+        }
+    }
+}
